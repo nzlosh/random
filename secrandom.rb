9397a4fcddc6042a660746a44bb9d6f6c3dee2be
phrawzty <phrawzty+github@gmail.com>
2012-11-06 18:16:24 +0100

    secrandom plaything

---
+++ secrandom.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require 'securerandom'
+
+# Things we should try to generate:
+# - A fixed number of only numbers.
+# - A fixed number of only low-bit ascii characters.
+# - A string of only characters that aren't confusing.
